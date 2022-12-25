a63b1f65beab1065d23e8f2722b714ece709c296
unknown <seletskiyweb@gmail.com>
2022-12-25 15:23:45 +0200

    New lang select withoud styles

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -938,3 +938,74 @@ anime({
   loop: true,
   easing: "easeInOutQuad",
 });
+
+const LANGUAGES = {
+  'Русский': 'RU',
+  'English': 'EN',
+  'Қазақ': 'KZ'
+}
+
+$("select.dropdown").each(function () {
+  var dropdown = $("<div />").addClass("dropdown selectDropdown");
+
+  $(this).wrap(dropdown);
+
+  var label = $("<span />")
+    .text($(this).attr("placeholder"))
+    .insertAfter($(this));
+    console.log(label);
+  var list = $("<ul />");
+
+  $(this)
+    .find("option")
+    .each(function () {
+      list.append($("<li />").append($("<a />").text($(this).text())));
+    });
+
+  list.insertAfter($(this));
+
+  if ($(this).find("option:selected").length) {
+    console.log($(this).find("option:selected").text());
+    label.text(LANGUAGES[$(this).find("option:selected").text()]);
+    list
+      .find("li:contains(" + $(this).find("option:selected").text() + ")")
+      .addClass("active");
+    $(this).parent().addClass("filled");
+  }
+});
+
+$(document).on("click touch", ".selectDropdown ul li a", function (e) {
+  e.preventDefault();
+  var dropdown = $(this).parent().parent().parent();
+  var active = $(this).parent().hasClass("active");
+  var label = active
+    ? dropdown.find("select").attr("placeholder")
+    : $(this).text();
+
+  dropdown.find("option").prop("selected", false);
+  dropdown.find("ul li").removeClass("active");
+
+  dropdown.toggleClass("filled", !active);
+  dropdown.children("span").text(LANGUAGES[label]);
+
+  if (!active) {
+    dropdown
+      .find("option:contains(" + $(this).text() + ")")
+      .prop("selected", true);
+    $(this).parent().addClass("active");
+  }
+
+  dropdown.removeClass("open");
+});
+
+$(".dropdown > span").on("click touch", function (e) {
+  var self = $(this).parent();
+  self.toggleClass("open");
+});
+
+$(document).on("click touch", function (e) {
+  var dropdown = $(".dropdown");
+  if (dropdown !== e.target && !dropdown.has(e.target).length) {
+    dropdown.removeClass("open");
+  }
+});
